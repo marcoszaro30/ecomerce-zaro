4dc8a02f5402ca1dc34a55c06c65bf35f6d3d3a7
marcos zaro <marcoszaro30@gmail.com>
2022-08-21 20:09:42 -0300

    entrega landing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar';
+import cards from './components/cards';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar></Navbar>
+        <Navbar/>
+        <cards/>
+        <cards/>
       </header>
     </div>
   );
